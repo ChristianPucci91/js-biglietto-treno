d7986b4a7f6c0ba729725fc5c25b11b311f9c646
ChristianPucci91 <pucci.christian91@gmail.com>
2020-10-20 17:01:29 +0200

    Esercizio Bonus done. add html & css style.

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,36 +1,32 @@
-// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// bonus - mettiamo in display none le icone e a seconda del comando di output si visualizzerà un'icona.
 
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+document.getElementById("over65").style.display = "none";
+document.getElementById("under18").style.display = "none";
+document.getElementById("standard").style.display = "none";
 
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65
-
-// chiedo l'età & i km all'utente
+// chiedo l'età & i km all'utente e creo le varie variabili di sconto.
 
 var eta = parseInt( prompt( " Quanti anni hai? " ));
 var km = parseInt( prompt( " Quanti km vuoi percorrere? " ));
-
-// prezzo biglietto ( 0.21€ per km * km inseriti dall'utente)
-
 var prezzoBiglietto = ( km * 0.21 );
-
-// prezzo biglietto scontato 20% ( prezzo intero - prezzo percentuale = prezzo scontato)
-
 var prezzoScontato20 = ( prezzoBiglietto - ( prezzoBiglietto * 0.20 ));
-
-// prezzo biglietto scontato 40% ( prezzo intero - prezzo percentuale = prezzo scontato)
-
 var prezzoScontato40 = ( prezzoBiglietto - ( prezzoBiglietto * 0.40 ));
 
 // Stabilire condizione di età
 
 if (eta <= 17) {
-  document.getElementById('biglietto').innerHTML = (" prezzo scontato al 20% = " + prezzoScontato20.toFixed(2) + "€") ;
+  document.getElementById('biglietto').innerHTML = ( prezzoScontato20.toFixed(2) + "€") ;
+  document.getElementById('titolo-prezzo').innerHTML = ("Tariffa under 18 applicata, il totale del biglietto è : ");
+  document.getElementById("under18").style.display = "block";
+
 } else if (eta >= 66) {
-   document.getElementById('biglietto').innerHTML = (" prezzo scontato al 40% = " + prezzoScontato40.toFixed(2) + "€" ) ;
+   document.getElementById('biglietto').innerHTML = ( prezzoScontato40.toFixed(2) + "€" ) ;
+   document.getElementById('titolo-prezzo').innerHTML = ("Tariffa over 65 applicata, il totale del biglietto è : ");
+   document.getElementById("over65").style.display = "block";
 } else {
-   document.getElementById('biglietto').innerHTML = (" prezzo intero = " + prezzoBiglietto.toFixed(2) + "€") ;
+   document.getElementById('biglietto').innerHTML = ( prezzoBiglietto.toFixed(2) + "€") ;
+   document.getElementById('titolo-prezzo').innerHTML = ("Tariffa standard applicata, il totale del biglietto è : ");
+   document.getElementById("standard").style.display = "block";
 }
 
 //.toFixed per arrotondare il prezzo scontato in difetto/eccesso.
